67c44d33c65694d2f43b0153ec87da23bab67a12
ahmeddxfouad <ahmed_fouad97@hotmail.com>
2026-02-19 23:45:09 +0200

    remove credentials

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@ export AUTH0_DOMAIN="your-tenant.eu.auth0.com"
 export API_AUDIENCE="casting-agency"
 export ALGORITHMS="RS256"
 
-export AUTH0_CLIENT_ID="YOUR_AUTH0_CLIENT_ID"       # optional but useful for reviewers
-export AUTH0_CLIENT_SECRET="YOUR_AUTH0_CLIENT_SECRET" # optional, do not commit real value
-
 export ASSISTANT_TOKEN="YOUR_ASSISTANT_JWT"
 export DIRECTOR_TOKEN="YOUR_DIRECTOR_JWT"
 export PRODUCER_TOKEN="YOUR_PRODUCER_JWT"
